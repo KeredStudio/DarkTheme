2697ac061a203a6073fcd2a783889a73acc31322
Derek Chang <cathay.derek@gmail.com>
2020-01-17 11:45:51 +0800

    feature A

--- Application/src/main/java/com/example/android/darktheme/MainActivity.java
+++ Application/src/main/java/com/example/android/darktheme/MainActivity.java
@@ -30,7 +30,7 @@ import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    String type = "A";
     private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
